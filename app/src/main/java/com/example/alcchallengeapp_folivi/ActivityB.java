c75bd3d087a4846db4a6920eba323fed438ddd54
Joshua Folivi <joshua.folivi@gmail.com>
2019-07-14 23:54:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alcchallengeapp_folivi/ActivityB.java
@@ -0,0 +1,70 @@
+package com.example.alcchallengeapp_folivi;
+
+import android.content.DialogInterface;
+import android.net.http.SslError;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ActivityB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+        WebView alcAboutPage = findViewById(R.id.about_alc_webview);
+        alcAboutPage.getSettings().setJavaScriptEnabled(true);
+        alcAboutPage.getSettings().setDomStorageEnabled(true);
+
+        //present alert dialog for user to enable or deny ssl verification
+        alcAboutPage.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(final WebView view, final SslErrorHandler handler, SslError error) {
+                Log.d("CHECK", "onReceivedSslError");
+                AlertDialog.Builder builder = new AlertDialog.Builder(ActivityB.this);
+                AlertDialog alertDialog = builder.create();
+                String message = "Certificate error.";
+                switch (error.getPrimaryError()) {
+                    case SslError.SSL_UNTRUSTED:
+                        message = "The certificate authority is not trusted.";
+                        break;
+                    case SslError.SSL_EXPIRED:
+                        message = "The certificate has expired.";
+                        break;
+                    case SslError.SSL_IDMISMATCH:
+                        message = "The certificate Hostname mismatch.";
+                        break;
+                    case SslError.SSL_NOTYETVALID:
+                        message = "The certificate is not yet valid.";
+                        break;
+                }
+                message += " Do you want to continue anyway?";
+                alertDialog.setTitle("SSL Certificate Error");
+                alertDialog.setMessage(message);
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("CHECK", "Button ok pressed");
+                        // Ignore SSL certificate errors
+                        handler.proceed();
+                    }
+                });
+                alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("CHECK", "Button cancel pressed");
+                        handler.cancel();
+                    }
+                });
+                alertDialog.show();
+            }
+        });
+        alcAboutPage.loadUrl("https://andela.com/alc/");
+
+    }
+}
